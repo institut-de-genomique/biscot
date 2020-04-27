60ef003573c12705745823b210a46d7de4c77dc2
bistace <bistace@genoscope.cns.fr>
2020-04-27 10:21:18 +0200

    Now using setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="biscot",
     packages=["biscot"],
     version="2.0",
+    entry_points={
+        "console_scripts": [
+            "biscot = biscot.biscot:run",
+        ],
+    },
     license="CeCILL",
     description="Bionano SCaffolding Correction Tool",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Benjamin Istace, Caroline Belser, Jean-Marc Aury",
     author_email="bistace@genoscope.cns.fr",
     url="https://github.com/institut-de-genomique/biscot",
-    download_url="https://github.com/institut-de-genomique/biscot/archive/v2.0.tar.gz",
+    download_url="https://github.com/institut-de-genomique/biscot/archive/v2.1.tar.gz",
     keywords=["Genome", "Scaffolding", "Bionano",],
     install_requires=["argparse", "biopython", "coloredlogs",],
     classifiers=[
